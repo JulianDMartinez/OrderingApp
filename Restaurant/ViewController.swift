8b1a6e86276971edca9664e09043db4d522f40d1
Julian Martinez <julian.martinez.s@outlook.com>
2021-04-10 13:50:07 -0400

    Initial Commit

---
+++ Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Julian Martinez on 4/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
